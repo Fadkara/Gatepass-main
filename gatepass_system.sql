4a26d327fe35ef0bfdbaa0aac97a5f1118b57689
Fadkara254 <oumafadhili8@gmail.com>
2025-08-04 16:20:10 +0300

    Updates to match db

--- gatepass_system.sql
+++ gatepass_system.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 28, 2025 at 02:16 PM
+-- Generation Time: Aug 04, 2025 at 03:18 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.1.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,6 +35,41 @@ CREATE TABLE `assets` (
   `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `assets`
+--
+
+INSERT INTO `assets` (`id`, `name`, `serial`, `type`, `user_id`) VALUES
+(1, 'Dell', '5cd741678g', 'Laptop', 1),
+(2, 'HP', '10620521', 'Laptop', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gate_logs`
+--
+
+CREATE TABLE `gate_logs` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `type` enum('Employee','Visitor') NOT NULL,
+  `purpose` varchar(255) DEFAULT NULL,
+  `time_in` datetime DEFAULT NULL,
+  `time_out` datetime DEFAULT NULL,
+  `asset_description` text DEFAULT NULL,
+  `asset_quantity` int(11) DEFAULT 1,
+  `asset_direction` enum('In','Out') DEFAULT 'Out'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `gate_logs`
+--
+
+INSERT INTO `gate_logs` (`id`, `name`, `type`, `purpose`, `time_in`, `time_out`, `asset_description`, `asset_quantity`, `asset_direction`) VALUES
+(11, 'Fadkara\'s Org', 'Visitor', 'Test', '2025-08-04 15:11:00', '2025-08-04 15:11:00', 'laptop', 1, 'In'),
+(12, 'Fadkara', 'Visitor', 'Test', '2025-08-04 15:32:00', '2025-08-04 15:32:00', 'KLS asset ', 1, 'In'),
+(13, 'Fadkara', 'Visitor', 'Test', '2025-08-04 15:32:00', '2025-08-04 15:32:00', 'KLS asset ', 1, 'In');
+
 -- --------------------------------------------------------
 
 --
@@ -43,10 +78,21 @@ CREATE TABLE `assets` (
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
+  `full_name` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
-  `user_type` enum('employee','visitor') DEFAULT NULL
+  `role` varchar(50) DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `full_name`, `email`, `role`, `status`, `created_at`) VALUES
+(1, 'Test One', 'vioke@admin.com', 'Visitor', 'Active', '2025-08-04 09:49:45'),
+(2, 'Siginon One', 'siginon@admin.com', 'Employee', 'Active', '2025-08-04 09:52:09'),
+(4, 'Test One', 'testy@gmail.com', 'Employee', 'Inactive', '2025-08-04 11:46:17');
 
 --
 -- Indexes for dumped tables
@@ -60,10 +106,17 @@ ALTER TABLE `assets`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Indexes for table `gate_logs`
+--
+ALTER TABLE `gate_logs`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -73,13 +126,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `assets`
 --
 ALTER TABLE `assets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `gate_logs`
+--
+ALTER TABLE `gate_logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
